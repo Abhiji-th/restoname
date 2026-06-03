9cd6085e239b2c4a8ae7796f3e0838508d7541d9
abhiji-th <abhijithc366@gmail.com>
2026-06-03 13:42:28 +0530

    Optimised code using chains

--- character_generator.py
+++ character_generator.py
@@ -26,10 +26,8 @@ template = PromptTemplate(
     partial_variables={"format_instruction": parser.get_format_instructions()}
 )
 
-prompt = template.format(place="japanese")
+chain = template | model | parser
 
-result = model.invoke(prompt)
+result = chain.invoke({"place": "indian"})
 
-final_result = parser.parse(result.content)
-
-print(final_result)
+print(result)
