db796fc315b89b3a772ed2faf22ad949d7689c22
abhiji-th <abhijithc366@gmail.com>
2026-06-03 13:40:50 +0530

    Update parser to pydantic parser

--- character_generator.py
+++ character_generator.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,7 +13,12 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm=llm)
 
-parser = JsonOutputParser()
+class Person(BaseModel):
+    name: str = Field(description="Name of the person")
+    age: int = Field(gt=18, lt=25, description="Age of the person", default=20)
+    city: str = Field(description="City the person belongs to")
+
+parser = PydanticOutputParser(pydantic_object=Person)
 
 template = PromptTemplate(
     template="Generate the name, age and city of a fictional {place} person. \n {format_instruction}",
@@ -24,4 +30,6 @@ prompt = template.format(place="japanese")
 
 result = model.invoke(prompt)
 
-print(result.content)
+final_result = parser.parse(result.content)
+
+print(final_result)
