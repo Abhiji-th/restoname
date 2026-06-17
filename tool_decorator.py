c61c04c2e9caf5fd94db4b74bfca5c1024a0177d
abhiji-th <abhijithc366@gmail.com>
2026-06-17 20:14:17 +0530

    Added pydantic check

--- tool_decorator.py
+++ tool_decorator.py
@@ -1,10 +1,19 @@
-from langchain_community.tools import tool
+from langchain_community.tools import StructuredTool
+from pydantic import BaseModel, Field
 
-@tool
+class MultiplyInput(BaseModel):
+    a: int = Field(required=True, description="This is the first number for multiplication")
+    b: int = Field(required=True, description="This is the second number for multiplication")
+
 def multiply(a: int, b: int) -> int:
     """Multiply two integers"""
     return a*b;
 
-result = multiply.invoke({'a': 2, 'b': 5})
+tool = StructuredTool.from_function(
+    func=multiply,
+    name="Multipy",
+    description="Multiply two integers",
+    args_schema=MultiplyInput
+)
 
-print(result)
+print(tool.invoke({'a': 2, 'b': 5}))
